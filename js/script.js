9f564e4427340f64ff8d247d0ef9cd7559067266
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-10 21:21:24 +1000

    add function and styles for scrolling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  
   const sidebar = document.querySelector('.sidebar');
   const menuButton = sidebar.querySelector('.sidebar__menu button');
   menuButton.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar--opened')
   })
+
+  const header = document.querySelector('.header');
+  window.addEventListener('scroll', () => {
+    if (window.innerWidth < 1440) {
+      if (window.scrollY > 45) {
+        header.classList.add('header--fixed');
+      } else {
+        header.classList.remove('header--fixed')
+      }
+    }
+  })
+  
+
+
 })
